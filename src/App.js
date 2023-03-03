ce4d342e020842d11469b48bf6d9f27be4b986ae
harsh jaiswal <hsjaiswal3110@gmail.com>
2023-03-03 21:45:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+// import { updateTodo } from "../../backend/controllers/ToDoControllers";
+import ToDo from "./components/ToDo";
+import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
 
 function App() {
+
+  const [toDo, setToDo] = useState([]);
+  const [text, setText] = useState("");
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [todoId, setTodoId] = useState("");
+  const [theme, setTheme] = useState("light");
+  const toggleTheme=()=>{
+    if(theme==='light'){
+      setTheme('dark');
+    }else{
+      setTheme('light');
+    }
+  }
+  useEffect(()=>{
+    getAllToDo(setToDo);
+  },[]);
+  const updateMode = (_id, text)=>{
+    if(isUpdate){
+      setIsUpdate(false);
+      setText("");
+      setTodoId("");
+    }else{
+      setIsUpdate(true);
+      setText(text);
+      setTodoId(_id); 
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${theme}App`}>
+      <div className="container">
+        <div className="heading">
+          <h1 className={`heading${theme}`}>ToDo App</h1> <input id="checkbox" className="toggleSwitch" type="checkbox" name="checkbox" onClick={toggleTheme}></input> 
+          <label className="label" for="checkbox"></label>
+        </div>
+        <div className="top">
+          <input className={theme} type="text" placeholder="Add ToDos..." value={text} onChange={(e)=>setText(e.target.value)}></input>
+            <div className={`add${theme}`} onClick={isUpdate ? () => updateToDo(todoId, text, setToDo, setText, setIsUpdate, Date(Date.now())) : ()=>addToDo(text, setText, setToDo, Date(Date.now()))}>{isUpdate ? "Update":"Add"}</div>
+        </div>
+        <div className="list">
+          {toDo.map((item)=> <ToDo key={item._id} text={item.text} lastEdit={item.lastEdit} theme={theme}
+          updateMode = {()=>updateMode(item._id, item.text)} 
+          deleteTodo = {()=>deleteToDo(item._id, setToDo)}
+          />)}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utils/HandleApi.js
@@ -0,0 +1,45 @@
+import axios from "axios"
+
+const baseUrl = "http://localhost:5000/"
+
+const getAllToDo = (setToDo)=>{
+    axios.get(baseUrl).then(({data})=>{
+        console.log(data);
+        setToDo(data);
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+const addToDo = (text, setText, setToDo, lastEdit)=>{
+    text = text.charAt(0).toUpperCase() + text.slice(1);
+    axios.post(baseUrl+"save", {text, lastEdit}).then(({data})=>{
+        console.log(data);
+        setText("");
+        getAllToDo(setToDo);
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdate, lastEdit)=>{
+    text = text.charAt(0).toUpperCase() + text.slice(1);
+    axios.post(baseUrl+"update", {_id:toDoId, text, lastEdit}).then(({data})=>{
+        console.log(data);
+        setText("");
+        setIsUpdate(false);
+        getAllToDo(setToDo);
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+const deleteToDo = (_id, setToDo)=>{
+    axios.post(baseUrl+"delete", {_id}).then(()=>{
+        getAllToDo(setToDo);
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+export {getAllToDo, addToDo, updateToDo, deleteToDo};
